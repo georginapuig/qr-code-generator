c8d1c846cb29217359eefa5519084b4f15396ff8
georgina puig <georginaxcodes@gmail.com>
2020-10-13 20:29:37 -0500

    add home action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,21 @@
 class PagesController < ApplicationController
+  require 'rqrcode'
+
+  def home
+    @qrcode = RQRCode::QRCode.new(params[:link].to_s)
+
+    @svg = @qrcode.as_svg(
+      offset: 0,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 6,
+      standalone: true
+    )
+  end
+
+  # private
+
+  # def link_params
+  #   params.require(:qrcode).permit(:link)
+  # end
 end
