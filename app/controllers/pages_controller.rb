8d7fe305a20214a269fc8dbf389afa0327b0236b
georgina puig <georginaxcodes@gmail.com>
2020-10-14 12:26:46 -0500

    change module size

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,18 @@ class PagesController < ApplicationController
   def home
     @qrcode = RQRCode::QRCode.new(params[:link].to_s)
 
-    @svg = @qrcode.as_svg(
-      offset: 0,
-      color: '000',
-      shape_rendering: 'crispEdges',
-      module_size: 6,
-      standalone: true
-    )
+    if params[:link].present?
+      @svg = @qrcode.as_svg(
+        offset: 0,
+        color: '000',
+        shape_rendering: 'crispEdges',
+        module_size: 4,
+        standalone: true
+      )
+    else
+      @svg = @qrcode.as_svg(
+        module_size: 7
+      )
+    end
   end
 end
