7287cc34e1f6ea440400c686e8eee67f5599bfb2
georgina puig <georginaxcodes@gmail.com>
2020-10-19 15:04:35 -0500

    add url var

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,8 @@ class PagesController < ApplicationController
 
   def qr
     # localhost:3000/websites/visit?url=https%3A%2F%2Fwww.google.com
-    @url = ENV=['URL'] + params[:url].to_s
+    @url_var = ENV=['URL']
+    @url = @url_var + params[:url].to_s
     @qrcode = RQRCode::QRCode.new(@url)
 
     if params[:url].present?
