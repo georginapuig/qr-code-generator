52caf2b1eaccd8b33e5dc9ae452e6da0fd2c0525
georgina puig <georginaxcodes@gmail.com>
2020-10-20 01:09:38 -0500

    add comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,13 @@ class PagesController < ApplicationController
   require 'rqrcode'
 
   def qr
-    @website = Website.create(url_params)
+    @website = Website.create(url: url_params[:url], visits: 0)
     
+    # if url is valid
     if @website.save
-      # https://rails-qr-code-generate.herokuapp.com/websites/visit?url= + https://www.google.com
+      # @url = https://rails-qr-code-generate.herokuapp.com/websites/visit?url= + https://www.google.com
       @url = ENV['URL'] + @website[:url].to_s
+      # create qr code
       @qrcode = RQRCode::QRCode.new(@url)
 
       @svg = @qrcode.as_svg(
