499399eacd5350183136fdfc508261af382a74eb
georgina puig <georginaxcodes@gmail.com>
2020-10-20 00:38:39 -0500

    validate url done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,21 +2,19 @@ class PagesController < ApplicationController
   require 'rqrcode'
 
   def qr
-    # https://rails-qr-code-generate.herokuapp.com/websites/visit?url= + https://www.google.com
-    @url = ENV['URL'] + url_params[:url].to_s
-    @qrcode = RQRCode::QRCode.new(@url)
+    @website = Website.create(url_params)
+    
+    if @website.save
+      # https://rails-qr-code-generate.herokuapp.com/websites/visit?url= + https://www.google.com
+      @url = ENV['URL'] + @website[:url].to_s
+      @qrcode = RQRCode::QRCode.new(@url)
 
-    if url_params.present?
       @svg = @qrcode.as_svg(
         offset: 0,
         color: '000',
         shape_rendering: 'crispEdges',
         module_size: 4,
         standalone: true
-      )
-    else
-      @svg = @qrcode.as_svg(
-        module_size: 5
       )
     end
   end
