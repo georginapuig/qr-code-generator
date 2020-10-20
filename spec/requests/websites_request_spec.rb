5f56ada1ed2a5a484eaa20f29d3a7c6e91a8e6a0
georgina puig <georginaxcodes@gmail.com>
2020-10-20 18:43:04 -0500

    rspec websites controller

---
+++ spec/requests/websites_request_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Websites", type: :request do
+  describe "GET websites#qr" do
+    it "should get qr" do
+      get '/websites/qr'
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET websites#qr" do
+    it "renders home" do
+      get '/websites/qr'
+      expect(response).to render_template("websites/qr")
+    end
+  end
+
+  describe "GET websites#visit" do
+    it "redirects to @url" do
+      get '/websites/visit'
+      expect(response).to redirect_to(assigns(:url)) 
+    end
+  end
+
+  context 'scope tests' do
+
+  end
+end
